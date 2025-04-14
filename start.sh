a2d34acae9a757e7e8636bb83f530e75734fd48e
Evandro Gardolin <evandrogardolinn@gmail.com>
2025-04-14 16:38:52 -0300

    fix: adiciona seed e migration no script de start

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 cd backend/ && docker-compose up -d
 cd ../frontend/ && docker-compose up -d
 cd ../
+
+docker exec api -it php artisan migrate:fresh --seed
+
 echo "Backend and frontend are up and running."
 echo "You can access the frontend at http://localhost:5173 and the backend at http://localhost:8000."
 echo "To stop the services, run ./stop.sh"
